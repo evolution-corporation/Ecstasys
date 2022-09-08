361282fe68b737d8fbeeb8a5d8bff40d73bdcf57
nikita <miner.hard00@gmail.com>
2022-09-08 16:27:39 +0500

    add ResultSubscribe, update

--- src/components/screens/Subscribe/ResultSubscribe/index.tsx
+++ src/components/screens/Subscribe/ResultSubscribe/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { View, Text, StyleSheet, Dimensions } from "react-native";
+import { ColorButton } from "~components/dump";
 
 import Tools from "~core";
 
@@ -9,29 +10,41 @@ const ResultSubscribeScreen = ({ route = { params: { status: "Edit" } } }) => {
   const { params } = route;
   return (
     <View style={styles.background}>
-      <Check />
-      <Text style={[styles.textTitle, { marginTop: 28 }]}>
-        {Tools.i18n.t("ready")}!
-      </Text>
-      <Text style={[styles.textTitle, { marginTop: 30, marginBottom: 11 }]}>
-        {Tools.i18n.t(
-          params.status === "Designations"
-            ? "36665b8a-3801-4b54-928a-22d9291e279d"
-            : params.status === "Edit"
-            ? "3c0d4097-a685-4141-9459-fdade7175b3b"
-            : "274347f0-628b-4128-8595-d6be9611ea03"
-        )}
-        !
-      </Text>
-      <Text style={styles.textDescription}>
-        {Tools.i18n.t(
-          params.status === "Designations"
-            ? "cd6eba3d-0d93-4e46-bb3c-d809ee76b181"
-            : params.status === "Edit"
-            ? "b952bec5-f5b6-439b-ab66-5e118673c19f"
-            : "2b2951bd-0ee8-42c7-b37e-2a7059596b6a"
-        )}
-      </Text>
+      <View style={{ flex: 1 }} />
+      <View style={styles.middleBlock}>
+        <Check />
+        <Text style={[styles.textTitle, { marginTop: 28 }]}>
+          {Tools.i18n.t("ready")}!
+        </Text>
+        <Text style={[styles.textTitle, { marginTop: 30, marginBottom: 11 }]}>
+          {Tools.i18n.t(
+            params.status === "Designations"
+              ? "36665b8a-3801-4b54-928a-22d9291e279d"
+              : params.status === "Edit"
+              ? "3c0d4097-a685-4141-9459-fdade7175b3b"
+              : "274347f0-628b-4128-8595-d6be9611ea03"
+          )}
+          !
+        </Text>
+        <Text style={styles.textDescription}>
+          {Tools.i18n.t(
+            params.status === "Designations"
+              ? "cd6eba3d-0d93-4e46-bb3c-d809ee76b181"
+              : params.status === "Edit"
+              ? "b952bec5-f5b6-439b-ab66-5e118673c19f"
+              : "2b2951bd-0ee8-42c7-b37e-2a7059596b6a"
+          )}
+        </Text>
+      </View>
+
+      <View style={styles.bottomBlock}>
+        <ColorButton
+          styleText={styles.colorButtonText}
+          styleButton={styles.colorButton}
+        >
+          {Tools.i18n.t("ready")}
+        </ColorButton>
+      </View>
     </View>
   );
 };
@@ -40,9 +53,9 @@ const styles = StyleSheet.create({
   background: {
     backgroundColor: "#9765A8",
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
   },
+  middleBlock: { justifyContent: "center", alignItems: "center", flex: 1 },
+  bottomBlock: { justifyContent: "center", flex: 1 },
   textTitle: {
     fontSize: 20,
     color: "#FFFFFF",
@@ -55,6 +68,14 @@ const styles = StyleSheet.create({
     width: "80%",
     maxWidth: Dimensions.get("screen").width - 40,
     textAlign: "center",
+  },
+  colorButtonText: {
+    color: "#FFFFFF",
+  },
+  colorButton: {
+    backgroundColor: "#702D87",
+    minWidth: 100,
+    alignSelf: "center",
   },
 });
 
