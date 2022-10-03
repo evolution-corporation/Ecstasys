c11e795cd979c528ab087b2a9d605078050efb92
Nikita <miner.hard00@gmail.com>
2022-10-03 13:55:48 +0500

    add support flipper. Need native code with support flipper

--- app.config.js
+++ app.config.js
@@ -1,55 +1,64 @@
-const IS_DEV = process.env.APP_VARIANT === 'development';
+import { version } from './package.json'
 
+function createAppConfig(){
+  const IS_DEV = process.env.APP_VARIANT === 'development';
 
-export default {
-  expo: {
-    jsEngine: "hermes",
-    name: `DMD Meditation${IS_DEV ? ' dev' : ''}`,
-    slug: "dmd_meditation",
-    version: "0.0.14",
-    orientation: "portrait",
-    githubUrl: "https://github.com/HardKot/ecstasys",
-    icon: "./assets/icon.png",
-    userInterfaceStyle: "light",
-    splash: {
-      image: "./assets/splash.png",
-      resizeMode: "contain",
-      backgroundColor: "#FFFFFF"
-    },
-    updates: {
-      fallbackToCacheTimeout: 0,
-      url: "https://u.expo.dev/65b7d342-ce66-4baa-9507-071987162a44"
-    },
-    assetBundlePatterns: ["**/*"],
-    ios: {
-      supportsTablet: true
-    },
-    android: {
-      adaptiveIcon: {
-        foregroundImage: "./assets/adaptive-icon.png",
-        backgroundColo: "#FFFFFF"
-      },
+  const appConfig = {
+    expo: {
+      jsEngine: "hermes",
+      name: `DMD Meditation${IS_DEV ? ' dev' : ''}`,
+      slug: "dmd_meditation",
+      version: version,
+      orientation: "portrait",
+      githubUrl: "https://github.com/HardKot/ecstasys",
       icon: "./assets/icon.png",
-      package: `com.ecorp.dmd_meditation${IS_DEV ? '_dev' : ''}`,
-      googleServicesFile: `./google-services.json`,
-      permissions: ["android.permission.RECORD_AUDIO"]
-    },
-    plugins: [
-      "expo-dev-client",
-      "expo-splash-screen",
-      "expo-image-picker",
-      "@react-native-firebase/app",
-      "expo-av",
-      "expo-updates",
-    ],
-    extra: {
-      eas: {
-        projectId: "65b7d342-ce66-4baa-9507-071987162a44"
+      userInterfaceStyle: "light",
+      splash: {
+        image: "./assets/splash.png",
+        resizeMode: "contain",
+        backgroundColor: "#FFFFFF"
       },
-      isDebug: IS_DEV
-    },
-    runtimeVersion: {
-      policy: "sdkVersion"
-    },
+      updates: {
+        fallbackToCacheTimeout: 0,
+        url: "https://u.expo.dev/65b7d342-ce66-4baa-9507-071987162a44"
+      },
+      assetBundlePatterns: ["**/*"],
+      ios: {
+        supportsTablet: true
+      },
+      android: {
+        adaptiveIcon: {
+          foregroundImage: "./assets/adaptive-icon.png",
+          backgroundColor: "#FFFFFF"
+        },
+        icon: "./assets/icon.png",
+        package: `com.ecorp.dmd_meditation${IS_DEV ? '_dev' : ''}`,
+        googleServicesFile: `./google-services.json`,
+        permissions: ["android.permission.RECORD_AUDIO"]
+      },
+      plugins: [
+        "expo-dev-client",
+        "expo-splash-screen",
+        "expo-image-picker",
+        "@react-native-firebase/app",
+        "expo-av",
+        "expo-updates",
+      ],
+      extra: {
+        eas: {
+          projectId: "65b7d342-ce66-4baa-9507-071987162a44"
+        },
+        isDebug: IS_DEV
+      },
+      runtimeVersion: {
+        policy: "sdkVersion"
+      },
+    }
   }
+
+  if (IS_DEV) {
+    appConfig.expo.plugins.push('expo-community-flipper')
+  }
+  return appConfig
 }
+export default createAppConfig()
