368bec67f4145b209e92ebbed61a469583c74c21
Nikita Batyukhno <nikita@batyukhno-dev.ru>
2023-04-02 14:14:24 +0500

    DMD-71 Добавить подписку на пол-года ios

--- app.config.ts
+++ app.config.ts
@@ -6,8 +6,7 @@ import { version } from "./package.json";
 function generateConfig(): ExpoConfig {
 	const appName =
 		process.env.APP_VARIANT === "dev" ? "DMD Dev" : process.env.APP_VARIANT === "prev" ? "DMD beta" : "DMD";
-	const appUrl =
-		process.env.APP_VARIANT === "dev" ? "com.evodigital.dmdmeditation+dev" : "com.evodigital.dmdmeditation";
+	const appUrl = "com.evodigital.dmdmeditation";
 	const toDay = new Date();
 	const date = {
 		date: toDay.getDate() < 10 ? "0" + toDay.getDate() : toDay.getDate(),
