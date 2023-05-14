9debf618823be217b14da9a7c71298e1d1f2cb5c
Nikita Batyukhno <nikita@batyukhno-dev.ru>
2023-05-14 15:38:04 +0500

    DMD-84

--- src/hooks/use-is-activate-subscribe.ts
+++ src/hooks/use-is-activate-subscribe.ts
@@ -6,18 +6,7 @@ import { adapty } from "react-native-adapty";
 import { useAppSelector } from "src/store/index";
 
 const useIsActivateSubscribe = () => {
-	 if (Platform.OS === "ios") {
-	 	const [subscribe, setSubscribe] = useState(false);
 
-	 	useEffect(() => {
-	 		adapty.addEventListener("onLatestProfileLoad", profile => {
-	 			const accessLevels = profile.accessLevels;
-	 			setSubscribe(accessLevels === undefined ? false : accessLevels.premium?.isActive ?? false);
-	 		});
-	 	}, []);
-
-	 	return subscribe;
-	 } else {
 		return useAppSelector(store => {
 			if (store.account.subscribe === undefined) {
 				return false;
@@ -47,6 +36,6 @@ const useIsActivateSubscribe = () => {
 			}
 		});
 	};
-};
+
 
 export default useIsActivateSubscribe;
