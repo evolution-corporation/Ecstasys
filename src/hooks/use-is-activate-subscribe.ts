752890670f75bda798420cedff9a819f6d764908
Nikita Batyukhno <nikita@batyukhno-dev.ru>
2023-06-09 08:18:51 +0500

    DMD-88

--- src/hooks/use-is-activate-subscribe.ts
+++ src/hooks/use-is-activate-subscribe.ts
@@ -1,8 +1,6 @@
 /** @format */
 
-import { useEffect, useState } from "react";
-import { Platform } from "react-native";
-import { adapty } from "react-native-adapty";
+
 import { useAppSelector } from "src/store/index";
 
 const useIsActivateSubscribe = () => {
@@ -11,27 +9,8 @@ const useIsActivateSubscribe = () => {
 			if (store.account.subscribe === undefined) {
 				return false;
 			} else {
-				const endSubscribe = new Date(store.account.subscribe.whenSubscribe);
+				const endSubscribe = new Date(store.account.subscribe.endSubscribe);
 
-				endSubscribe.setDate(
-					endSubscribe.getDate() +
-						(() => {
-							switch (store.account.subscribe.type) {
-								case "WEEK": {
-									return 7;
-								}
-								case "MONTH": {
-									return 30;
-								}
-								case "HALF_YEAR": {
-									return 180;
-								}
-								default: {
-									return 0;
-								}
-							}
-						})()
-				);
 				return endSubscribe.getTime() > Date.now();
 			}
 		});
